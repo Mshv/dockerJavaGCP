31bda9cb207a13b63c7826374996ddae3bfb37fa
mehdi shahveri <mehdi.shahverdi@axiata.com>
2019-04-23 15:17:44 +0800

    chnage pom

--- src/main/java/aac/sample/dockerjavagcp/DockerJavaGcpApplication.java
+++ src/main/java/aac/sample/dockerjavagcp/DockerJavaGcpApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DockerJavaGcpApplication {
 
-  @RequestMapping("/")
+  @RequestMapping("/mehdi")
   public String home() {
     return "Hello Docker Cloud World!";
   }
